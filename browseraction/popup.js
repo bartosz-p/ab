876f6d4db0bfd8d208045c037b8b85c96c8cda18
bpiechnik <bartoszpiechnik@yahoo.com>
2014-07-14 20:58:24 +0200

    Initial commit

---
+++ browseraction/popup.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+	document.getElementById("button").onclick = function() {
+		chrome.extension.sendMessage({
+	        type: "color-divs"
+	    });
+	}
+}
+
+/*
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"}, function(response) {});
+});
+
+*/
+
+/*
+
+// getting the URL of the current tab
+var my_url = window.location
+window.onload = function() {
+    document.getElementById("url_button").onclick = function() {
+        chrome.extension.sendMessage({
+            type: "my_url",
+            data: "lalalalal"
+        });
+    }
+}
+
+*/
---
+++ content.js
@@ -0,0 +1,18 @@
+/*
+
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+	switch(message.type) {
+		case "colors-div":
+			var divs = document.querySelectorAll("div");
+			if(divs.length === 0) {
+				alert("There are no divs in the page.");
+			} else {
+				for(var i=0; i<divs.length; i++) {
+					divs[i].style.backgroundColor = message.color;
+				}
+			}
+		break;
+	}
+});
+
+*/
