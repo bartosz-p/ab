fd106b5537fa42f50047e0a90f19ca8732f8320a
bpiechnik <bartoszpiechnik@yahoo.com>
2014-07-19 16:55:07 +0200

    comment popups on every link

--- content.js
+++ content.js
@@ -19,27 +19,80 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
     var divs = document.getElementsByTagName("div");
     var headers = document.getElementsByTagName('h2');
 
-    console.log(links.length, divs.length, headers.length)
+    console.log(links.length, divs.length, headers.length);
+
+    var imageUrl = chrome.extension.getURL("icons/comments_icon.png");
+
+    function PlaceImage(source_x, source_y, imageUrl) {
+        var newImage = document.createElement("img");
+        newImage.setAttribute("src", imageUrl);
+
+
+
+        // This part is done with getBoundingClientRect()
+        /*
+        var imagePosition = newImage.getBoundingClientRect();
+        imagePosition.left = source_x;
+        imagePosition.top = source_y;
+        */
+
+        //var image_y = imagePosition.top;
+        //image_x = source_x;
+        //image_y = source_y;
+
+        document.body.appendChild(newImage);
+
+        // This part will set an image position. The function should retrieve pos x and y of a source element as arguments.
+
+        newImage.style.position = "absolute";
+        newImage.style.left = source_x + 'px';
+        newImage.style.top = source_y + 'px';
+
+        //newImage.setAttribute('height', '1px');
+        //newImage.setAttribute('width', '1px');
+    }
 
     // Links
+    // TO-DO: make small icons to pop-up in the left, top corner of each element
     for(var i=0; i<links.length; i++) {
         //links[i].style.border = true;
         //links[i].style.borderWidth = "20";
-        links[i].style.background = "blue";
+
+        // Color the element
+        //links[i].style.background = "blue";
+
+        // Get the position of an element with 'style'
+        //var x = links[i].style.left;
+        //var y = links[i].style.top;
+        //var x = i;
+        //var y = i;
+
+        // Get the position of an element with getBoundingClientRect
+
+        var position = links[i].getBoundingClientRect();
+        var x = position.left;
+        var y = position.top;
+        y -=32;
+
+
+        // Create comment image
+        PlaceImage(x, y, imageUrl);
+
+        //links[i].style.position = "absolute";
+        console.log('x: ', x, 'y: ', y);
     }
 
     // Divs
     for(var j=0; j<divs.length; j++) {
         //divs[j].style.border = true;
         //divs[j].style.borderWidth = "20";
-        divs[j].style.background = "green";
+        //divs[j].style.background = "green";
     }
 
     // Headers
     for(var k=0; k<headers.length; k++) {
-        headers[k].style.background = "red";
+        //headers[k].style.background = "red";
     }
-
 });
 
 
